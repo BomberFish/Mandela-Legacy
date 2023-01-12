946edafc45f80e63896cfb269b546905cb1c7978
BomberFish <87151697+BomberFish@users.noreply.github.com>
2023-01-12 13:20:33 -0500

    Who needs a NavigationStack anyways?

--- Mandela/ContentView.swift
+++ Mandela/ContentView.swift
@@ -37,23 +37,10 @@ struct ListView: View {
     }
 }
 
-struct MainNav<Content>: View where Content: View {
-    @ViewBuilder var content: () -> Content;
-    
-    var body: some View {
-        if #available(iOS 16, *) {
-            NavigationStack(root: content)
-        } else {
-            NavigationView(content: content)
-        }
-    }
-}
-
-
 struct ContentView: View {
     @Binding var triggerRespring: Bool
     var body: some View {
-        MainNav {
+        NavigationView {
             ListView()
                 .toolbar {
                     Button{
