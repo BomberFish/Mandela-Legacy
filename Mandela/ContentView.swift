1141f66a3f826ae76ed168ada8acb5974522bbb7
BomberFish <87151697+BomberFish@users.noreply.github.com>
2023-01-12 08:39:12 -0500

    Add listview

--- Mandela/ContentView.swift
+++ Mandela/ContentView.swift
@@ -7,13 +7,23 @@
 
 import SwiftUI
 
+struct ListView: View {
+    var body: some View {
+        List {
+            Text("Hello, world!")
+            Text("Hello, world!")
+            Text("Hello, world!")
+            Text("Hello, world!")
+            Text("Hello, world!")
+        }
+        .navigationTitle("Mandela")
+    }
+}
+
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            ListView()
         }
         .padding()
     }
