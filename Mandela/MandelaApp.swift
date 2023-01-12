2a3a3751ba95775e562ded676b26516cd5fcae87
BomberFish <87151697+BomberFish@users.noreply.github.com>
2023-01-12 08:22:42 -0500

    Initial Commit

---
+++ Mandela/MandelaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MandelaApp.swift
+//  Mandela
+//
+//  Created by Hariz Shirazi on 2023-01-12.
+//
+
+import SwiftUI
+
+@main
+struct MandelaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
