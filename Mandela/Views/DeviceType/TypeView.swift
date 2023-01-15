3d7278bf6d6f85451ee39b6493347eefb5b2bb80
BomberFish <87151697+BomberFish@users.noreply.github.com>
2023-01-14 23:34:21 -0500

    overall goofyness

--- Mandela/Views/DeviceType/TypeView.swift
+++ Mandela/Views/DeviceType/TypeView.swift
@@ -22,10 +22,10 @@ struct TypeView: View {
         .controlSize(.large)
         .tint(.blue)
         .buttonStyle(.bordered)
-        .onAppear {
-        let alert = UIAlertController(title: "Warning", message: "This tweak has a chance of damaging your device. By using this tweak, you agree that I (BomberFish) am not responsible for any damage that happens to your device as a result of using this tweak.", preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: "Ok", style: .destructive, handler: nil))
-        UIApplication.shared.windows.first?.rootViewController?.present(alert, animated: true, completion: nil)
+        //.onAppear {
+        //let alert = UIAlertController(title: "Warning", message: "This tweak has a chance of damaging your device. By using this tweak, you agree that I (BomberFish) am not responsible for any damage that happens to your device as a result of using this tweak.", preferredStyle: .alert)
+        //alert.addAction(UIAlertAction(title: "Ok", style: .destructive, handler: nil))
+        //UIApplication.shared.windows.first?.rootViewController?.present(alert, animated: true, completion: nil)
         }
             
             Button{
