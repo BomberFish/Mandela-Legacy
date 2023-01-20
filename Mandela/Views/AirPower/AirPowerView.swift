1aa5f948787eda2207ee9e6f4df516f130a98980
BomberFish <87151697+BomberFish@users.noreply.github.com>
2023-01-20 08:06:45 -0500

    AirPower

---
+++ Mandela/Views/AirPower/AirPowerView.swift
@@ -0,0 +1,20 @@
+//
+//  AirPowerView.swift
+//  Mandela
+//
+//  Created by Hariz Shirazi on 2023-01-20.
+//
+
+import SwiftUI
+
+struct AirPowerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AirPowerView_Previews: PreviewProvider {
+    static var previews: some View {
+        AirPowerView()
+    }
+}
