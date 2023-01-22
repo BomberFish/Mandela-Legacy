d8c51c2a8105314be33407cdbe7ccd957c6990bc
BomberFish <87151697+BomberFish@users.noreply.github.com>
2023-01-21 23:02:54 -0500

    Whoops

--- Mandela/Views/DeviceType/TypeView.swift
+++ Mandela/Views/DeviceType/TypeView.swift
@@ -93,29 +93,28 @@ struct TypeView: View {
         }
     }
     // MARK: - plist editing function
-    func plistChange(plistPath: String, key: String, value: Int) {
-        let stringsData = try! Data(contentsOf: URL(fileURLWithPath: plistPath))
-        
-        let plist = try! PropertyListSerialization.propertyList(from: stringsData, options: [], format: nil) as! [String: Any]
-        func changeValue(_ dict: [String: Any], _ key: String, _ value: Int) -> [String: Any] {
-            var newDict = dict
-            for (k, v) in dict {
-                if k == key {
-                    newDict[k] = value
-                } else if let subDict = v as? [String: Any] {
-                    newDict[k] = changeValue(subDict, key, value)
-                }
-            }
-            return newDict
-        }
-   
-        var newPlist = plist
-        newPlist = changeValue(newPlist, key, value)
+func plistChange(plistPath: String, key: String, value: Int) {
+    let stringsData = try! Data(contentsOf: URL(fileURLWithPath: plistPath))
     
-        let newData = try! PropertyListSerialization.data(fromPropertyList: newPlist, format: .binary, options: 0)
-
-        overwriteFile(newData, plistPath)
+    let plist = try! PropertyListSerialization.propertyList(from: stringsData, options: [], format: nil) as! [String: Any]
+    func changeValue(_ dict: [String: Any], _ key: String, _ value: Int) -> [String: Any] {
+        var newDict = dict
+        for (k, v) in dict {
+            if k == key {
+                newDict[k] = value
+            } else if let subDict = v as? [String: Any] {
+                newDict[k] = changeValue(subDict, key, value)
+            }
+        }
+        return newDict
     }
+    
+    var newPlist = plist
+    newPlist = changeValue(newPlist, key, value)
+    
+    let newData = try! PropertyListSerialization.data(fromPropertyList: newPlist, format: .binary, options: 0)
+    
+    overwriteFile(newData, plistPath)
 }
 
 struct TypeView_Previews: PreviewProvider {
